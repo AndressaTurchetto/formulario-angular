ee7c3aad1f2bae07aa380997d530f96173b0c455
andressa turchetto <and.turchetto@gmail.com>
2024-04-23 15:35:27 -0300

    Estudando sobre Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColor1: '#5F6F52',
+        customColor2: '#A9B388',
+        customColor3: '#B99470',
+        customAzul: '#002F52',
+      },
+    },
   },
-  variants: {},
   plugins: [],
 }
+
